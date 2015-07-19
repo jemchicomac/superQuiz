49feb0e9ed73848df43aaa9557286280fec940d4
antonio @ jemchicomac <antonio@jemchicomac.com>
2015-07-19 20:29:31 +0200

    [EXAMEN P2P] Seccion Acerca de mi

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Super Quiz' });
 });
 
+/* GET Authors. */
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'Acerca de' });
+});
+
 module.exports = router;
